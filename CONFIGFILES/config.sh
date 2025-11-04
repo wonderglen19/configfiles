3c4c79c9592cfd0abea0121bf70d9f6e50d7a4e7
wonderglen19 <glendys.filsinger@gmail.com>
2025-11-04 13:09:59 +0100

    adding new configuration for my mac

--- CONFIGFILES/config.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-CONFIGFILES=(.bash_profile .gitconfig .gitignore .zshrc)
-
-for configfile in $(echo ${CONFIGFILES[*]});
-do
-    cp ~/CONFIGFILES/$(echo $configfile) ~/$(echo $configfile)
-done
